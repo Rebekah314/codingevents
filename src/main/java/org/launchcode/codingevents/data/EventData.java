792bdb43d0c0e94037e3bcf978fb8a7f17216a1a
Rebekah Garris <rebekahlgarris@gmail.com>
2023-10-09 17:58:28 -0500

    finished chapter examples

---
+++ src/main/java/org/launchcode/codingevents/data/EventData.java
@@ -0,0 +1,36 @@
+package org.launchcode.codingevents.data;
+
+import org.launchcode.codingevents.models.Event;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EventData {
+
+    //Need a place to put events
+    private static final Map<Integer, Event> events = new HashMap<>();
+
+    //static methods (no instances required)
+
+    //add an event:
+    public static void add(Event event) {
+        events.put(event.getId(), event);
+    }
+
+    //get a single event:
+    public static Event getById(int id) {
+        return events.get(id);
+    }
+
+    //get all events:
+    public static Collection<Event> getAll() {
+        return events.values();
+    }
+
+    //remove an event:
+    public static void remove(int id) {
+        events.remove(id);
+    }
+
+}
